7beba37c4114b21cedccab520f05f38775bbddee
svonme <svon.me@gmail.com>
2023-02-10 16:00:52 +0800

    feat: Add pic

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@
 
 
 import lib from "./lib/index";
+import Pic from "./components/icon/pic.vue";
 import Enum from "./components/enum/index.vue";
 export { IconExpand } from "./components/icon/expand";
 
@@ -12,6 +13,6 @@ export { default as Icon } from "./components/icon";
 export { default as Link } from "./components/link";
 export { default as Time } from "./components/time";
 
-export { Enum };
+export { Enum, Pic };
 
 export default lib;
