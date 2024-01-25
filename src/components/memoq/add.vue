4323bcc2182e3d71eb1afe5fc4d34f99bb83a6c4
TheresiaT <37453744@qq.com>
2024-01-25 14:15:54 +0800

    feat:业财版本前Memoq设置的调整

--- src/components/memoq/add.vue
+++ src/components/memoq/add.vue
@@ -6,28 +6,37 @@
 
 import * as _ from "lodash-es";
 import { PropType, ref, onMounted } from "vue";
-import { Button, Table, Space, Input, Empty } from "ant-design-vue";
+import { Button, Table, Input, Empty, Form, FormItem, RangePicker } from "ant-design-vue";
 import { table } from "@ue/utils";
 import Time from "../time";
 import { ToolProjectData, headMemoq } from "./type";
 import * as message from "@ue/message";
 import SourceTarget from "../source/target.vue";
+import Dayjs from 'dayjs';
 
 const props = defineProps({
   // 请求数据列表接口
   listApi: {
     required: true,
-    type: Function as PropType<(name: string) => Promise<ToolProjectData[]>>,
+    type: Function as PropType<(object: { projectName?: string, startDate?: string, endDate?: string }) => Promise<ToolProjectData[]>>,
+  },
+  // BUM列表
+  memoqNamesList: {
+    required: true,
+    type: Array as PropType<string[]>,
   },
 });
 //复选框
 const { selected, rowSelection } = table.useSelection();
 
-const projectName = ref("")
+const searchParams = ref<{ timeArr: [string, string], projectName: string }>({
+  projectName: "",
+  timeArr: [Dayjs().add(-1, 'month').format('YYYY-MM-DD'), Dayjs().format('YYYY-MM-DD')]
+})
 
 //loading状态
 const loading = ref(false)
-const searchVal = ref("")//用来判断筛选条件是否存在
+const isSearch = ref(true)//用来判断筛选条件是否存在
 //memoq列表
 let memoqList = ref()
 //获取数据列表
@@ -36,8 +45,13 @@ const getData = async function () {
   memoqList.value = [];
   if (props.listApi && typeof props.listApi === "function") {
     try {
-      searchVal.value = projectName.value
-      memoqList.value = await Promise.resolve(props.listApi(projectName.value)) || [];
+      isSearch.value = !!searchParams.value.projectName || !!searchParams.value.timeArr
+      const data = {
+        projectName: searchParams.value.projectName,
+        startDate: searchParams.value.timeArr?.[0],
+        endDate: searchParams.value.timeArr?.[1],
+      }
+      memoqList.value = await Promise.resolve(props.listApi(data)) || [];
     } catch (error) { }
   } else {
     message.error("请传入获取数据的API方法")
@@ -55,12 +69,23 @@ defineExpose({ submit: onSubmit });
 </script>
 <template>
   <div>
-    <Space>
-      <Input v-model:value="projectName" @keyup.enter="getData" :maxlength="64" placeholder="Search project name"
-        allow-clear class="w-100" />
-      <Button @click="getData" type="primary">Search</Button>
-    </Space>
-    <div v-if="_.size(memoqList) < 1 && !searchVal && !loading" class="min-h-100 flex items-center justify-center">
+    <div class="bg-[#f6f6f6] px-4 py-3 mb-3">
+      Your ERP account have been linked the memoQ account: <span class="font-semibold">{{_.concat(memoqNamesList).join(',')}}</span> .
+    </div>
+    <Form ref="formRef" layout="vertical" class="grid gap-x-6 grid-cols-3">
+      <FormItem label="Project Name">
+        <Input v-model:value="searchParams.projectName" @keyup.enter="getData" :maxlength="64"
+          placeholder="Search project name" allow-clear />
+      </FormItem>
+      <FormItem label="Start Date">
+        <RangePicker v-model:value="searchParams.timeArr" format="YYYY-MM-DD" value-format="YYYY-MM-DD" />
+      </FormItem>
+      <FormItem label="&nbsp;">
+        <Button @click="getData" type="primary">Search</Button>
+      </FormItem>
+    </Form>
+
+    <div v-if="_.size(memoqList) < 1 && !isSearch && !loading" class="min-h-100 flex items-center justify-center">
       <Empty>
         <template #description>
           Create project in <a target="_blank" href="//memoq.ecinnovations.com/memoqweb">memoq server</a> or client
@@ -68,7 +93,7 @@ defineExpose({ submit: onSubmit });
       </Empty>
     </div>
     <template v-else>
-      <Table :row-selection="rowSelection" class="mt-6 mb-4" :columns="headMemoq" rowKey="catProjectName"
+      <Table :row-selection="rowSelection" class="mb-4" :columns="headMemoq" rowKey="catProjectName"
         :loading="loading" :scroll="{ y: 460, scrollToFirstRowOnChange: true }" :data-source="memoqList"
         :pagination="false">
         <template #bodyCell="{ column, record, text }">
